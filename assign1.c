9cc2668bb2500f91ec78a160a4c7e28f4579c0ae
Parth-Dave <parthdave718@gmail.com>
2019-07-02 00:52:55 +0530

    added sh_read defination

--- assign1.c
+++ assign1.c
@@ -3,9 +3,32 @@
 #include <unistd.h>
 #include <string.h>
 #define DIR_BUF_SIZE 256
+#define READ_BUF_SIZE 1024
 bool on_flag=0;
 bool log_flag=0;
 char *dir = malloc(sizeof(char)*DIR_BUF_SIZE);
+
+char *sh_read(){
+	int bufsize=READ_BUF_SIZE;
+	int position=0;
+	char *buffer=malloc(sizeof(char)*bufsize);
+	char c;
+	if(!buffer){
+		printf("sh_read allocation error\n");
+	}
+	while(1){
+		c=getchar();
+
+		if(c==EOF||c=='\n'){
+			buffer[position]='\0';
+			return buffer;
+		}
+		else{
+			buffer[position]=c;
+		}
+		position++;
+	}
+}
 
 int main(int argc,char **argv){
 	char *line;
